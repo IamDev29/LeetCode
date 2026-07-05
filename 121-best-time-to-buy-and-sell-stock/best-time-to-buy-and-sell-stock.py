5c21e8b3cc36167e8a5e72e25a5d19b78c7e5e63
Dev <ankitkumar.bebartta@gmail.com>
2026-07-05 13:23:20 +0530

    Time: 158 ms (5.50%) | Memory: 19.4 MB (27.97%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -7,7 +7,7 @@ class Solution(object):
         mini=prices[0]
         profit=0
 
-        for i in range(len(prices)):
+        for i in range(1,len(prices)):
             cost=prices[i]-mini
             profit=max(profit,cost)
             mini=min(mini,prices[i])
